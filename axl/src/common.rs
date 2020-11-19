f074a840d6787f341c0f8d6d5d0dc1cc72ac7be7
Zoey <Dessix@Dessix.net>
2020-11-19 00:46:36 -0800

    Added stream serialization for MetaStreamHeader

--- axl/src/common.rs
+++ axl/src/common.rs
@@ -10,11 +10,13 @@ use quinn::{
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::{
   path::{Path, PathBuf},
+  pin::Pin,
   sync::Arc,
   task::{Context, Poll},
 };
+use serde::{Serialize, Deserialize};
 
-#[derive(Clone, Eq, PartialEq, Debug)]
+#[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub struct MetaStreamHeader {}
 
 impl MetaStreamHeader {
@@ -22,11 +24,23 @@ impl MetaStreamHeader {
     MetaStreamHeader {}
   }
 
-  pub async fn read_from_stream(_s: &mut quinn::RecvStream) -> Result<MetaStreamHeader> {
-    unimplemented!()
+  pub async fn read_from_stream(mut s: Pin<&mut dyn tokio::io::AsyncRead>) -> Result<MetaStreamHeader> {
+    use tokio::io::AsyncReadExt;
+    let length = s.read_u32().await.context("Failed to read stream header frame length")? as usize;
+    let mut buffer = Vec::with_capacity(length);
+    buffer.resize_with(length, Default::default);
+    s.read_exact(buffer.as_mut_slice()).await.context("Failed reading stream header frame contents")?;
+    let buffer = buffer.into_boxed_slice();
+    serde_json::from_slice::<MetaStreamHeader>(&buffer)
+      .context("Error decoding stream header json")
   }
 
-  pub async fn write_to_stream(_s: &mut quinn::SendStream) -> Result<()> {
-    unimplemented!()
+  pub async fn write_to_stream(&self, mut s: Pin<&mut dyn tokio::io::AsyncWrite>) -> Result<()> {
+    use tokio::io::AsyncWriteExt;
+    let buffer = serde_json::to_vec(&self)
+      .context("Error serializing stream header")?
+      .into_boxed_slice();
+    s.write_u32(buffer.len() as u32).await.context("Failed writing header frame length to stream")?;
+    s.write_all(&buffer).await.context("Failed writing header to stream")
   }
 }
