6ccd47a8bfdd49133025b755b0e0fd18234aee35
Zoey <Dessix@Dessix.net>
2020-11-19 01:14:18 -0800

    Added a unit test for MetaStreamHeader serialization

--- axl/src/common.rs
+++ axl/src/common.rs
@@ -24,7 +24,7 @@ impl MetaStreamHeader {
     MetaStreamHeader {}
   }
 
-  pub async fn read_from_stream(mut s: Pin<&mut dyn tokio::io::AsyncRead>) -> Result<MetaStreamHeader> {
+  pub async fn read_from_stream<T : tokio::io::AsyncRead + Unpin>(s: &mut T) -> Result<MetaStreamHeader> {
     use tokio::io::AsyncReadExt;
     let length = s.read_u32().await.context("Failed to read stream header frame length")? as usize;
     let mut buffer = Vec::with_capacity(length);
@@ -35,12 +35,35 @@ impl MetaStreamHeader {
       .context("Error decoding stream header json")
   }
 
-  pub async fn write_to_stream(&self, mut s: Pin<&mut dyn tokio::io::AsyncWrite>) -> Result<()> {
+  pub async fn write_to_stream<T : tokio::io::AsyncWrite + Unpin>(&self, s: &mut T) -> Result<()> {
     use tokio::io::AsyncWriteExt;
     let buffer = serde_json::to_vec(&self)
       .context("Error serializing stream header")?
       .into_boxed_slice();
     s.write_u32(buffer.len() as u32).await.context("Failed writing header frame length to stream")?;
     s.write_all(&buffer).await.context("Failed writing header to stream")
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::common::MetaStreamHeader;
+
+  #[async_std::test]
+  async fn stream_header_serialization_roundtrip() {
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    use std::pin::Pin;
+    let buffer: Vec<u8> = Vec::new();
+    let mut cursor = std::io::Cursor::new(buffer);
+    let original = MetaStreamHeader::new();
+    original
+      .write_to_stream(&mut cursor)
+      .await
+      .expect("Writing to stream must succeed");
+    cursor.set_position(0);
+    let deserialized = MetaStreamHeader::read_from_stream(&mut cursor)
+      .await
+      .expect("Reading header from stream must succeed");
+    assert_eq!(original, deserialized);
   }
 }
