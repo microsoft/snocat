d89a9dc391a4f664c71d148361a9b198b7f63a05
Zoey <Dessix@Dessix.net>
2020-11-21 11:41:41 -0800

    Remove some leftovers from prior refactoring

--- axl/src/lib.rs
+++ axl/src/lib.rs
@@ -5,38 +5,8 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use anyhow::{anyhow, bail, Context as AnyhowContext, Result};
-// #[macro_use]
-use async_std::io::{BufReader, BufWriter};
-use async_std::net::{TcpListener, TcpStream, ToSocketAddrs};
-use async_std::prelude::*;
-use futures::future::Either;
-use futures::{self, Future, FutureExt, *};
-use quinn::TransportConfig;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::{
-  path::{Path, PathBuf},
-  sync::Arc,
-  task::{Context, Poll},
-};
-use tracing::{error, info, info_span, trace};
-use tracing_futures::Instrument as _;
-
 pub mod common;
 pub mod util;
 
 pub mod client;
 pub mod server;
-
-use util::validators::{
-  parse_socketaddr, validate_existing_file, validate_ipaddr, validate_port_range,
-  validate_socketaddr,
-};
-
-#[cfg(test)]
-mod tests {
-  #[async_std::test]
-  async fn stream_one_byte() {
-    use async_std;
-  }
-}
