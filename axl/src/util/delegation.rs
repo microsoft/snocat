e03ae5f80cb100046783821e506c3db534902f10
Zoey <Dessix@Dessix.net>
2020-11-25 03:45:50 -0800

    Delegation is now working, albeit rather unsafe

--- axl/src/util/delegation.rs
+++ axl/src/util/delegation.rs
@@ -1,35 +1,37 @@
 //! Utilities for handling transfer of ownership of a future to an external resolver, and
 //! tracking the ouutstanding tasks without blocking.
 
-use std::sync::Arc;
 use futures::future::BoxFuture;
-use tokio::sync::{oneshot, Mutex};
 use futures::future::{Future, FutureExt};
-use tokio::sync::oneshot::error::RecvError;
-use std::task::{Context, Poll};
+use pin_project::pin_project;
 use std::pin::Pin;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+use tokio::sync::oneshot::error::RecvError;
+use tokio::sync::{oneshot, Mutex};
 
 #[derive(Default, Debug)]
-pub struct DelegatedTask {
-}
+pub struct DelegatedTask {}
 
 #[derive(Default, Debug)]
 struct DelegationSet {
   next_id: std::sync::atomic::AtomicU64,
-  items: std::collections::BTreeMap<u64, DelegatedTask>
+  items: std::collections::BTreeMap<u64, DelegatedTask>,
 }
 
 impl DelegationSet {
   fn gen_next_delegation_id(&self) -> u64 {
     // `Relaxed` because we don't need any specific ordering, only uniqueness via atomicity
-    self.next_id.fetch_add(1u64, std::sync::atomic::Ordering::Relaxed)
+    self
+      .next_id
+      .fetch_add(1u64, std::sync::atomic::Ordering::Relaxed)
   }
 
   pub fn attach_new(&mut self, delegated_task: DelegatedTask) -> u64 {
     let next_id = self.gen_next_delegation_id();
     match self.items.insert(next_id, delegated_task) {
       None => next_id,
-      Some(_replaced) => unreachable!("Duplicate key registered into delegation set")
+      Some(_replaced) => unreachable!("Duplicate key registered into delegation set"),
     }
   }
 
@@ -52,50 +54,67 @@ pub struct DelegationPool {
   delegations: Mutex<DelegationSet>,
 }
 
-#[derive(Debug)]
-pub struct DelegatedReceiver<
-  'a,
-  'b : 'a,
-  T: Send + 'b,
-> {
+pub struct DelegatedReceiver<'a, 'b: 'a, T: Send + 'b> {
+  task_id: u64,
   pool: &'a DelegationPool,
   receiver: oneshot::Receiver<T>,
-  task_id: u64,
-  // lta: std::marker::PhantomData<&'a DelegationPool>,
+  received: Option<(Result<T, oneshot::error::RecvError>, BoxFuture<'a, ()>)>,
   ltb: std::marker::PhantomData<&'b DelegationPool>,
 }
 
-impl<
-  'a,
-  'b : 'a,
-  T: Send + 'b,
-> Future for DelegatedReceiver<'a, 'b, T> {
+impl<'a, 'b: 'a, T: Send + 'b> std::fmt::Debug for DelegatedReceiver<'a, 'b, T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "[DTaskR #{}]", &self.task_id)
+  }
+}
+
+impl<'a, 'b: 'a, T: Send + 'b> Future for DelegatedReceiver<'a, 'b, T> {
   type Output = Result<T, DelegationError>;
 
   fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
     let task_id = self.task_id;
     let pool = self.pool;
-    let recv = unsafe {
-      self.map_unchecked_mut(|x| &mut x.receiver)
-    };
-    match recv.poll(cx) {
-      Poll::Ready(res) => {
-        pool.detach(&task_id);
-        Poll::Ready(match res {
-          Ok(t) => Ok(t),
-          Err(_) => {
-            // Dispatcher was dropped before sending anything
-            Err(DelegationError::DispatcherDropped)
+    if self.received.is_none() {
+      let (received, recv) = unsafe {
+        use std::ops::DerefMut;
+        let raw = Pin::into_inner_unchecked(self);
+        (&mut raw.received, Pin::new_unchecked(&mut raw.receiver))
+      };
+      match recv.poll(cx) {
+        Poll::Ready(res) => {
+          use futures::future::FutureExt;
+          let mut detacher = pool.detach(task_id).boxed();
+          match detacher.poll_unpin(cx) {
+            Poll::Ready(_) => {
+              // In case the detach was synchronous, we provide a fast-path with no state-save
+              Poll::Ready(res.map_err(|_| DelegationError::DispatcherDropped))
+            }
+            Poll::Pending => {
+              *received = Some((res, detacher));
+              Poll::Pending
+            }
           }
-        })
+        }
+        Poll::Pending => Poll::Pending,
       }
-      Poll::Pending => Poll::Pending,
+    } else {
+      let raw = unsafe { Pin::into_inner_unchecked(self) };
+      let (res, mut detacher) = std::mem::replace(&mut raw.received, None).unwrap();
+      match detacher.poll_unpin(cx) {
+        Poll::Ready(_) => {
+          // If there was an error, the dispatcher handle was dropped before sending anything
+          Poll::Ready(res.map_err(|_| DelegationError::DispatcherDropped))
+        }
+        Poll::Pending => {
+          let _ = std::mem::replace(&mut raw.received, Some((res, detacher)));
+          Poll::Pending
+        }
+      }
     }
   }
 }
 
 impl DelegationPool {
-
   pub fn new() -> DelegationPool {
     DelegationPool {
       delegations: Default::default(),
@@ -104,14 +123,14 @@ impl DelegationPool {
 
   pub fn delegate<
     'a,
-    'b : 'a,
-    'c : 'b,
-    T : Sized + Send + 'c,
-    FutDispatch: futures::future::Future<Output=()> + Send + 'a
+    'b: 'a,
+    'c: 'b,
+    T: Sized + Send + 'c,
+    FutDispatch: futures::future::Future<Output = ()> + Send + 'a,
   >(
     &'b self,
     dispatch: impl (FnOnce(oneshot::Sender<T>) -> FutDispatch) + Send + 'a,
-  ) -> impl Future<Output=DelegatedReceiver<T>> + 'a {
+  ) -> impl Future<Output = DelegatedReceiver<T>> + 'a {
     let (dispatcher, promise) = oneshot::channel::<T>();
 
     async move {
@@ -124,16 +143,18 @@ impl DelegationPool {
         pool: self,
         task_id,
         receiver: promise,
+        received: None,
         ltb: std::marker::PhantomData,
       }
     }
   }
 
-  pub(super) async fn detach(&self, task_id: &u64) -> () {
+  pub(super) async fn detach(&self, task_id: u64) -> () {
     let mut lock = self.delegations.lock().await;
-    lock.detach(task_id).expect("Value must not be detached by another source");
+    lock
+      .detach(&task_id)
+      .expect("Value must not be detached by another source");
   }
-
 }
 
 #[cfg(test)]
@@ -142,19 +163,27 @@ mod tests {
   #[tokio::test]
   async fn delegation_sync() {
     let pool = super::DelegationPool::new();
-    let res = pool.delegate(async move |dispatch| {
-      dispatch.send(42).unwrap();
-    }).await.await.unwrap();
+    let res = pool
+      .delegate(async move |dispatch| {
+        dispatch.send(42).unwrap();
+      })
+      .await
+      .await
+      .unwrap();
     assert_eq!(res, 42);
   }
 
   #[tokio::test]
   async fn delegation_async() {
     let pool = super::DelegationPool::new();
-    let res = pool.delegate(async move |dispatch| {
-      tokio::task::yield_now().await;
-      dispatch.send(42).unwrap();
-    }).await.await.unwrap();
+    let res = pool
+      .delegate(async move |dispatch| {
+        tokio::task::yield_now().await;
+        dispatch.send(42).unwrap();
+      })
+      .await
+      .await
+      .unwrap();
     assert_eq!(res, 42);
   }
 
@@ -163,19 +192,37 @@ mod tests {
     let pool = super::DelegationPool::new();
     let mut dispatcher = None;
     let dispatched = {
-      pool.delegate(|dispatch| async {
+      let dispatched = pool.delegate(|dispatch| async {
         tokio::task::yield_now().await;
         dispatcher = Some(dispatch);
-      })
-    }.await;
+      });
+      assert_eq!(
+        pool.delegations.lock().await.get_outstanding_ids().len(),
+        0,
+        "Must not register until dispatched"
+      );
+      dispatched
+    }
+    .await;
 
     {
       let dispatcher = dispatcher.unwrap();
       dispatcher.send(42).unwrap();
     }
 
+    assert_eq!(
+      pool.delegations.lock().await.get_outstanding_ids().len(),
+      1,
+      "Must register active tasks"
+    );
+
     let res = dispatched.await.unwrap();
     assert_eq!(res, 42);
-  }
 
+    assert_eq!(
+      pool.delegations.lock().await.get_outstanding_ids().len(),
+      0,
+      "Must not leak tasks"
+    );
+  }
 }
