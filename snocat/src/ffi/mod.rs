64a0f8b381b200e78fa60b29f841b9b178ccbf7f
Zoey <Dessix@Dessix.net>
2020-12-21 21:12:44 -0800

    Allowed remote allocation of Rust-owned strings

--- snocat/src/ffi/mod.rs
+++ snocat/src/ffi/mod.rs
@@ -1,7 +1,7 @@
 //! Bindings for instantiation and control via C ABI
 
 use lazy_static::lazy_static;
-use ffi_support::{ConcurrentHandleMap, define_bytebuffer_destructor, define_handle_map_deleter, define_string_destructor, implement_into_ffi_by_json, HandleError, FfiStr, ExternError, IntoFfi};
+use ffi_support::{ConcurrentHandleMap, define_bytebuffer_destructor, define_handle_map_deleter, define_string_destructor, implement_into_ffi_by_json, HandleError, FfiStr, ExternError, IntoFfi, rust_string_to_c};
 use crate::{
   common::authentication::{self, DelegatedAuthenticationHandler},
   server::{
@@ -31,7 +31,29 @@ pub mod errors;
 pub mod dto;
 
 define_bytebuffer_destructor!(snocat_free_buffer);
-define_string_destructor!(snocat_free_string);
+// define_string_destructor!(snocat_free_string);
+// console-tracing version for development, to track free calls
+#[no_mangle]
+pub unsafe extern "C" fn snocat_free_string(s: *mut std::os::raw::c_char) {
+  ::ffi_support::abort_on_panic::with_abort_on_panic(|| {
+    println!("Freeing string handle {:?} with value {:?}", &s, if s.is_null() { "" } else {
+      let sffi = FfiStr::from_raw(s);
+      sffi.as_str()
+    });
+    if !s.is_null() {
+      ::ffi_support::destroy_c_string(s)
+    }
+  });
+}
+#[no_mangle]
+pub unsafe extern "C" fn snocat_alloc_string(s: FfiStr) {
+  ::ffi_support::abort_on_panic::with_abort_on_panic(|| {
+    let rs: *mut std::os::raw::c_char = rust_string_to_c(s.as_str());
+    println!("Rust-alloc string handle {:?} with value {:?}", &rs, s.as_str());
+    rs
+  });
+}
+
 
 struct ServerHandle<T : TunnelManager>(Option<Box<ConcurrentDeferredTunnelServer<T>>>, u32);
 impl<T : TunnelManager> Drop for ServerHandle<T> {
@@ -69,12 +91,33 @@ pub extern "C" fn snocat_server_start(
     use std::convert::TryInto;
     let config = serde_json::from_str::<dto::ServerConfig>(config_json.as_str())?;
     let config: quinn::ServerConfig = config.try_into()?;
-    println!("HELLO WORLD FROM C API");
+    println!("HELLO WORLD FROM C API with cfg {:?}", config);
     // let server = ConcurrentDeferredTunnelServer::new(TcpTunnelManager::new(
     //   Range::new(8000, 8010),
     //   IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
     //   todo!()
     // ));
     Ok(ServerHandle(None, 0))
+  })
+}
+
+#[no_mangle]
+pub extern "C" fn snocat_return_owned_string(
+  // error: &mut ExternError
+) -> *mut std::os::raw::c_char {
+  let mut error = ExternError::success();
+  let res = ffi_support::call_with_result::<String, errors::FfiError, _>(&mut error, || {
+    Ok("Hello world from rust! Delete me!".into())
+  });
+  unsafe { error.manually_release() }
+  res
+}
+
+#[no_mangle]
+pub extern "C" fn snocat_return_owned_string_err(
+  error: &mut ExternError
+) -> *mut std::os::raw::c_char {
+  ffi_support::call_with_result::<String, errors::FfiError, _>(error, || {
+    Ok("Hello world from rust! Delete me!".into())
   })
 }
