afc49b1b250796a4734110f1c5bc3b475f4454a1
Zoey <Dessix@Dessix.net>
2021-01-19 23:19:19 -0800

    Made TunnelStream compatible with futures-rs AsyncRead / AsyncWrite

--- snocat/src/util/tunnel_stream.rs
+++ snocat/src/util/tunnel_stream.rs
@@ -27,6 +27,15 @@ pub struct QuinnTunnelStream<TSession: quinn::crypto::Session>(
 );
 
 impl<TSession: quinn::crypto::Session> QuinnTunnelStream<TSession> {
+  pub fn new(
+    streams: (
+      quinn::generic::SendStream<TSession>,
+      quinn::generic::RecvStream<TSession>,
+    ),
+  ) -> Self {
+    Self(streams.0, streams.1)
+  }
+
   pub fn as_ref_tunnel_stream(&mut self) -> QuinnTunnelRefStream<TSession> {
     QuinnTunnelRefStream(&mut self.0, &mut self.1)
   }
@@ -102,3 +111,71 @@ impl<TSession: quinn::crypto::Session> AsyncRead for QuinnTunnelStream<TSession>
 
 impl<TSession: quinn::crypto::Session> TunnelStream for QuinnTunnelRefStream<'_, TSession> {}
 impl<TSession: quinn::crypto::Session> TunnelStream for QuinnTunnelStream<TSession> {}
+
+/// futures-rs compatibility for TunnelStream
+mod futures_traits {
+  use super::QuinnTunnelRefStream;
+  use super::QuinnTunnelStream;
+  use crate::util::tunnel_stream::TunnelStream;
+  use futures::{AsyncRead, AsyncWrite};
+  use std::io::Error as IOError;
+  use std::pin::Pin;
+  use std::task::{Context, Poll};
+  use tokio::io::DuplexStream;
+
+  impl<TSession: quinn::crypto::Session> futures::io::AsyncWrite for QuinnTunnelStream<TSession> {
+    fn poll_write(
+      mut self: Pin<&mut Self>,
+      cx: &mut Context<'_>,
+      buf: &[u8],
+    ) -> Poll<Result<usize, IOError>> {
+      AsyncWrite::poll_write(Pin::new(&mut (*self).0), cx, buf)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), IOError>> {
+      AsyncWrite::poll_flush(Pin::new(&mut (*self).0), cx)
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), IOError>> {
+      AsyncWrite::poll_close(Pin::new(&mut (*self).0), cx)
+    }
+  }
+
+  impl<TSession: quinn::crypto::Session> futures::io::AsyncRead for QuinnTunnelStream<TSession> {
+    fn poll_read(
+      mut self: Pin<&mut Self>,
+      cx: &mut Context<'_>,
+      buf: &mut [u8],
+    ) -> Poll<Result<usize, IOError>> {
+      tokio::io::AsyncRead::poll_read(Pin::new(&mut (*self).1), cx, buf)
+    }
+  }
+
+  impl futures::io::AsyncWrite for dyn TunnelStream {
+    fn poll_write(
+      self: Pin<&mut Self>,
+      cx: &mut Context<'_>,
+      buf: &[u8],
+    ) -> Poll<Result<usize, IOError>> {
+      tokio::io::AsyncWrite::poll_write(self, cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), IOError>> {
+      tokio::io::AsyncWrite::poll_flush(self, cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), IOError>> {
+      tokio::io::AsyncWrite::poll_shutdown(self, cx)
+    }
+  }
+
+  impl futures::io::AsyncRead for dyn TunnelStream {
+    fn poll_read(
+      self: Pin<&mut Self>,
+      cx: &mut Context<'_>,
+      buf: &mut [u8],
+    ) -> Poll<Result<usize, IOError>> {
+      tokio::io::AsyncRead::poll_read(self, cx, buf)
+    }
+  }
+}
