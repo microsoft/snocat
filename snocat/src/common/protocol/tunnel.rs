33e8db270364b6bb73ddce7f461b24a62530f6d0
Zoey <Dessix@Dessix.net>
2021-03-10 20:10:32 -0800

    DuplexTunnel test improvements
    Now we also test for DuplexTunnel concurrency capabilities

--- snocat/src/common/protocol/tunnel.rs
+++ snocat/src/common/protocol/tunnel.rs
@@ -170,23 +170,198 @@ impl TunnelIncoming {
 
 #[cfg(test)]
 mod tests {
+  use std::sync::Arc;
+
+  use futures::{AsyncReadExt, AsyncWriteExt};
+  use tokio::io::AsyncWrite;
+
   #[tokio::test]
   async fn duplex_tunnel() {
     use super::Tunnel;
     use futures::StreamExt;
     let ((a_tun, a_inc), (b_tun, b_inc)) = super::duplex();
 
-    a_tun.open_link().await.unwrap();
-    drop(a_tun);
-    let count_of_b: u32 = super::TunnelIncoming::streams(b_inc)
-      .fold(0, async move |memo, _stream| memo + 1)
-      .await;
-    assert_eq!(count_of_b, 2); // stream then ConnectionClosed event
-    b_tun.open_link().await.unwrap();
-    drop(b_tun);
-    let count_of_a: u32 = super::TunnelIncoming::streams(a_inc)
-      .fold(0, async move |memo, _stream| memo + 1)
-      .await;
-    assert_eq!(count_of_a, 2); // stream then ConnectionClosed event
+    let fut = async move {
+      a_tun.open_link().await.unwrap();
+      drop(a_tun); // Dropping the A tunnel ends the incoming streams for B
+      let count_of_b: u32 = super::TunnelIncoming::streams(b_inc)
+        .fold(0, async move |memo, _stream| memo + 1)
+        .await;
+      assert_eq!(count_of_b, 2); // stream then ConnectionClosed event
+      b_tun.open_link().await.unwrap();
+      drop(b_tun); // Dropping the B tunnel ends the incoming streams for A
+      let count_of_a: u32 = super::TunnelIncoming::streams(a_inc)
+        .fold(0, async move |memo, _stream| memo + 1)
+        .await;
+      assert_eq!(count_of_a, 2); // stream then ConnectionClosed event
+    };
+    tokio::time::timeout(std::time::Duration::from_secs(5), fut)
+      .await
+      .expect("DuplexTunnel test may be failing due to an await deadlock");
+  }
+
+  #[tokio::test]
+  async fn duplex_tunnel_concurrency() {
+    use super::{Tunnel, TunnelIncomingType};
+    use crate::util::tunnel_stream::TunnelStream;
+    use futures::{future, FutureExt, StreamExt};
+    use std::time::Duration;
+    use tokio::{sync::Mutex, time::timeout};
+    let (server, client) = super::duplex();
+
+    // For this test, the server will listen for two incoming streams,
+    // echo the content down new outgoing streams, and stop listening.
+    //
+    // Note that DuplexTunnel's "Incoming", unlike a Quinn connection,
+    // provides bistreams in the order they are opened by the tunnels,
+    // not the order in which they have their first data sent on them.
+    let fut_server = async move {
+      let (tun, inc) = server; // Explicit move
+      let tun_ref = &tun;
+      inc
+        .streams()
+        .take(2) // Expect two incoming streams in a row
+        .for_each_concurrent(None, async move |stream| {
+          let incoming_stream = match stream {
+            TunnelIncomingType::Closed(_) => {
+              panic!("Test connection closed before producing 2 streams")
+            }
+            TunnelIncomingType::BiStream(stream) => stream,
+          };
+          let (mut incoming_downlink, _incoming_uplink) = tokio::io::split(incoming_stream);
+          let (_outgoing_downlink, mut outgoing_uplink) =
+            tokio::io::split(tun_ref.open_link().await.unwrap());
+          crate::util::proxy_tokio_stream(&mut incoming_downlink, &mut outgoing_uplink)
+            .await
+            .unwrap();
+        })
+        .await;
+    };
+    // The client will attempt to make 3 streams, and will expect that the third will produce no result
+    let fut_client = async move {
+      let (tun, inc) = client; // Explicit move
+      let inc_streams = Arc::new(Mutex::new(inc.streams().boxed()));
+      const CLIENT_TASK_DURATION: Duration = Duration::from_secs(5);
+
+      // Async Barriers are used to ensure that we are making new
+      // streams in order and able to fulfill them them out-of-order
+      use tokio::sync::Barrier;
+      let step_1 = Barrier::new(2);
+      let step_2 = Barrier::new(2);
+      let step_3 = Barrier::new(3);
+      let step_4 = Barrier::new(3);
+
+      let client_a = {
+        let (tun, inc_streams) = (&tun, Arc::clone(&inc_streams));
+        let (step_1, step_2, step_3, step_4) = (&step_1, &step_2, &step_3, &step_4);
+        let task = async move {
+          let test_data_a = vec![1, 2, 3, 4];
+          use tokio::prelude::io::BufWriter;
+          let mut s: Box<dyn TunnelStream> = Box::new(tun.open_link().await.unwrap());
+          s.write_all(test_data_a.as_slice()).await.unwrap();
+          AsyncWriteExt::flush(&mut s).await.unwrap();
+          // Wait until B finishes starting a stream and sending on it, then receive a stream
+          println!("a1");
+          step_1.wait().await;
+          let inc = inc_streams
+            .lock()
+            .await
+            .next()
+            .await
+            .expect("Server must produce one stream per stream sent");
+          let mut downlink = match inc {
+            TunnelIncomingType::Closed(_) => panic!("Server closed before responding to stream"),
+            TunnelIncomingType::BiStream(stream) => stream,
+          };
+          // We've received a stream, wait until B receives its own before dropping our write-end
+          println!("a2");
+          step_2.wait().await;
+          println!("a3");
+          step_3.wait().await;
+          drop(s); // Dropping our write-end allows the echo server to complete the downlink
+          let mut buf = Vec::new();
+          downlink.read_to_end(&mut buf).await.unwrap();
+          assert_eq!(&buf, &test_data_a);
+          println!("a4");
+          step_4.wait().await;
+        };
+        task
+      };
+
+      let client_b = {
+        let (tun, inc_streams) = (&tun, Arc::clone(&inc_streams));
+        let (step_1, step_2, step_3, step_4) = (&step_1, &step_2, &step_3, &step_4);
+        let task = async move {
+          let test_data_b = vec![4, 3, 2];
+          use tokio::prelude::io::BufWriter;
+          // Wait until A has started a stream to ensure ordering
+          println!("b1");
+          step_1.wait().await;
+          let mut s: Box<dyn TunnelStream> = Box::new(tun.open_link().await.unwrap());
+          s.write_all(test_data_b.as_slice()).await.unwrap();
+          AsyncWriteExt::flush(&mut s).await.unwrap();
+          // Wait until A has received a stream before receiving our own
+          println!("b2");
+          step_2.wait().await;
+          let inc = inc_streams
+            .lock()
+            .await
+            .next()
+            .await
+            .expect("Server must produce one stream per stream sent");
+          let mut downlink = match inc {
+            TunnelIncomingType::Closed(_) => panic!("Server closed before responding to stream"),
+            TunnelIncomingType::BiStream(stream) => stream,
+          };
+          drop(s);
+          println!("b3");
+          step_3.wait().await;
+          // Wait for C to affirm that no further connections are being provided
+          println!("b4");
+          step_4.wait().await;
+          let mut buf = Vec::new();
+          downlink.read_to_end(&mut buf).await.unwrap();
+          assert_eq!(&buf, &test_data_b);
+        };
+        task
+      };
+
+      let client_c = {
+        let (step_3, step_4) = (&step_3, &step_4);
+        let inc_streams = Arc::clone(&inc_streams);
+        let task = async move {
+          println!("c1 (skipped)\nc2 (skipped)");
+          println!("c3");
+          step_3.wait().await;
+          let last = inc_streams.lock().await.next().await.unwrap();
+          assert!(matches!(last, TunnelIncomingType::Closed(_)));
+          println!("c4");
+          step_4.wait().await;
+        };
+        task
+      };
+
+      match timeout(
+        CLIENT_TASK_DURATION,
+        future::join3(client_a, client_b, client_c),
+      )
+      .await
+      {
+        Ok(_) => (),
+        Err(_timeout) => {
+          eprintln!(
+            "Client barrier status: {:#?} {:#?} {:#?} {:#?}",
+            &step_1, &step_2, &step_3, &step_4
+          );
+          panic!("Client timeout");
+        }
+      }
+    };
+    timeout(
+      Duration::from_secs(10),
+      future::join(fut_server, fut_client),
+    )
+    .await
+    .expect("Server/client test has apparent await deadlock");
   }
 }
