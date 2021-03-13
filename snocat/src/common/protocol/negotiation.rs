78e70d8b79ae7f20b7a84d335009e8fc52760ae6
Zoey <Dessix@Dessix.net>
2021-03-12 19:03:42 -0800

    Tracing and a test for the Negotiation protocol
    Fixed negotiation client misbehaviour where the client sent a delimited
    frame instead of raw bytes for the opening magic sequence.

--- snocat/src/common/protocol/negotiation.rs
+++ snocat/src/common/protocol/negotiation.rs
@@ -5,6 +5,7 @@ use futures::{
   Future,
 };
 use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tracing_futures::Instrument;
 
 use crate::util::tunnel_stream::{TunnelStream, WrappedStream};
 
@@ -29,7 +30,8 @@ async fn write_magic_and_version<S: AsyncWrite + Send + Unpin>(
   mut stream: S,
   protocol_version: u8,
 ) -> Result<S, NegotiationError> {
-  crate::util::framed::write_frame(&mut stream, SNOCAT_NEGOTIATION_MAGIC)
+  stream
+    .write_all(SNOCAT_NEGOTIATION_MAGIC)
     .await
     .map_err(|_| NegotiationError::WriteError)?;
   stream
@@ -60,8 +62,10 @@ fn protocol_magic<'a, S: TunnelStream + Send + 'a>(
       .await
       .map_err(|_| NegotiationError::ProtocolViolation)?;
     if remote_magic_len < remote_magic.len() || &remote_magic != SNOCAT_NEGOTIATION_MAGIC {
+      tracing::trace!("magic mismatch");
       return Err(NegotiationError::ProtocolViolation);
     }
+    tracing::trace!("magic matched expectation");
     Result::<_, NegotiationError>::Ok(read)
   };
 
@@ -74,12 +78,17 @@ fn protocol_magic<'a, S: TunnelStream + Send + 'a>(
       .map_err(|_| NegotiationError::ReadError)?;
     Ok(remote_version)
   }
+  .instrument(tracing::trace_span!(stringify!(protocol_magic)))
   .boxed()
 }
 
 pub struct NegotiationClient;
 
 impl NegotiationClient {
+  pub fn new() -> Self {
+    Self {}
+  }
+
   pub fn handle(
     self,
     addr: RouteAddress,
@@ -89,22 +98,29 @@ impl NegotiationClient {
     async move {
       // Absolute most-basic negotiation protocol - sends the address in a frame and waits for 0u8-or-fail
 
-      let remote_protocol_version = protocol_magic(&mut link, LOCAL_PROTOCOL_VERSION).await?;
+      tracing::trace!("performing negotiation protocol handshake");
+      let remote_version = protocol_magic(&mut link, LOCAL_PROTOCOL_VERSION).await?;
       // TODO: Consider adding a confirmation for negotiation protocol acceptance here
 
       // TODO: support multiple versions of negotiation mechanism
-      if remote_protocol_version > 0 {
+      if remote_version > 0 {
         // We don't support anything beyond this basic protocol yet
+        tracing::trace!(
+          version = remote_version,
+          "unsupported remote protocol version"
+        );
         return Err(NegotiationError::UnsupportedProtocolVersion);
       }
 
       // TODO: support service/client protocol versioning (May require Service/Client cooperation)
 
+      tracing::trace!("writing address");
       // Write address to the remote, and see if the requested protocol is supported
       crate::util::framed::write_frame(&mut link, &addr.into_bytes())
         .await
         .map_err(|_| NegotiationError::WriteError)?;
 
+      tracing::trace!("awaiting remote protocol service acceptance");
       // Await acceptance of address by a service, or refusal if none are compatible
       let accepted = link
         .read_u8()
@@ -112,24 +128,34 @@ impl NegotiationClient {
         .map_err(|_| NegotiationError::ReadError)?;
       if accepted > 0 {
         // For v0, this byte doesn't carry any useful info beyond accepted or not
+        tracing::trace!(
+          code = accepted,
+          "address refused by remote protocol services"
+        );
         Err(NegotiationError::Refused)
       } else {
+        tracing::trace!("address accepted by remote protocol services");
         Ok(link)
       }
     }
+    .instrument(tracing::trace_span!("Client"))
     .boxed()
   }
 }
 
-pub struct NegotiationServer<ServiceRegistry> {
+pub struct NegotiationService<ServiceRegistry> {
   service_registry: Arc<ServiceRegistry>,
 }
 
 pub type ArcService = Arc<dyn Service + Send + Sync + 'static>;
 
-impl<R> NegotiationServer<R> {}
+impl<R> NegotiationService<R> {
+  pub fn new(service_registry: Arc<R>) -> Self {
+    Self { service_registry }
+  }
+}
 
-impl<R> NegotiationServer<R>
+impl<R> NegotiationService<R>
 where
   R: ServiceRegistry + Send + Sync + 'static,
 {
@@ -146,11 +172,16 @@ where
     const CURRENT_PROTOCOL_VERSION: u8 = 0u8;
     let service_registry = Arc::clone(&self.service_registry);
     async move {
+      tracing::trace!("performing negotiation protocol handshake");
       let remote_version = protocol_magic(&mut link, CURRENT_PROTOCOL_VERSION).await?;
       // TODO: Consider adding a confirmation for negotiation protocol acceptance here
 
       if remote_version > 0 {
         // This should map to multiple supported versions, where possible
+        tracing::trace!(
+          version = remote_version,
+          "unsupported remote protocol version"
+        );
         return Err(NegotiationError::UnsupportedProtocolVersion);
       }
 
@@ -160,12 +191,14 @@ where
 
       let addr = String::from_utf8(addr).map_err(|_| NegotiationError::ProtocolViolation)?; // Addresses must be valid UTF-8
 
+      tracing::trace!("searching service registry for address handlers");
       let found = service_registry.find_service(&addr);
 
       match found {
         None => {
           // Write refusal
           // v0 calls for a non-zero u8 to be written to the stream to refuse an address
+          tracing::trace!("refusing address");
           link
             .write_u8(1)
             .await
@@ -175,6 +208,7 @@ where
         Some(service) => {
           // Write acceptance
           // v0 calls for a 0u8 to be written to the stream to accept an address
+          tracing::trace!("accepting address");
           link
             .write_u8(0)
             .await
@@ -183,13 +217,84 @@ where
         }
       }
     }
+    .instrument(tracing::trace_span!("Service"))
     .boxed()
   }
 }
 
 #[cfg(test)]
 mod tests {
+  use std::{sync::Arc, time::Duration};
+  use tokio::time::timeout;
+
+  use super::{ArcService, NegotiationClient, NegotiationError, NegotiationService};
+  use crate::common::protocol::{traits::ServiceRegistry, Service};
+  use crate::util::tunnel_stream::WrappedStream;
+
+  struct TestServiceRegistry {
+    services: Vec<ArcService>,
+  }
+
+  impl ServiceRegistry for TestServiceRegistry {
+    fn find_service(
+      self: std::sync::Arc<Self>,
+      addr: &crate::common::protocol::RouteAddress,
+    ) -> Option<std::sync::Arc<dyn crate::common::protocol::Service + Send + Sync + 'static>> {
+      self
+        .services
+        .iter()
+        .find(|s| s.accepts(addr))
+        .map(Arc::clone)
+    }
+  }
+
+  struct NoOpServiceAcceptAll;
+
+  impl Service for NoOpServiceAcceptAll {
+    fn accepts(&self, _addr: &crate::common::protocol::RouteAddress) -> bool {
+      true
+    }
+
+    fn handle(
+      &'_ self,
+      _addr: crate::common::protocol::RouteAddress,
+      _tunnel: Box<dyn crate::util::tunnel_stream::TunnelStream + Send + 'static>,
+    ) -> futures::future::BoxFuture<'_, Result<(), crate::common::protocol::ServiceError>> {
+      use futures::FutureExt;
+      futures::future::ready(Ok(())).boxed()
+    }
+  }
+
   /// Test that negotiation between client and server sends an address successfully
   #[tokio::test]
-  async fn negotiate() {}
+  async fn negotiate() {
+    let collector = tracing_subscriber::fmt()
+      .pretty()
+      .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+      .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+      .finish();
+    tracing::subscriber::set_global_default(collector).expect("Logger init must succeed");
+    const TEST_ADDR: &str = "/test/addr";
+    let service_registry = TestServiceRegistry {
+      services: vec![Arc::new(NoOpServiceAcceptAll)],
+    };
+    let service = NegotiationService::new(Arc::new(service_registry));
+    let client = NegotiationClient::new();
+    let (client_stream, server_stream) = WrappedStream::duplex(8192);
+
+    let client_future = async move {
+      let _stream = client.handle(TEST_ADDR.into(), client_stream).await?;
+      Result::<_, NegotiationError>::Ok(())
+    };
+
+    let server_future = async move {
+      // server
+      let (_stream, addr, service) = service.negotiate(server_stream).await?;
+      Result::<_, NegotiationError>::Ok((addr, service))
+    };
+    let fut = futures::future::try_join(client_future, server_future);
+    let fut = timeout(Duration::from_secs(5), fut);
+    let ((), (addr, _service)) = fut.await.expect("Must not time out").unwrap();
+    assert_eq!(addr.as_str(), TEST_ADDR);
+  }
 }
