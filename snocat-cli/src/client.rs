3bec97b032b52c0a68b5ad3d3b75f3ffc9731e45
Zoey <Dessix@Dessix.net>
2021-03-17 23:15:38 -0700

    Updated snocat-cli to use the new authentication model

--- snocat-cli/src/client.rs
+++ snocat-cli/src/client.rs
@@ -1,19 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license OR Apache 2.0
 use anyhow::{Context as AnyhowContext, Error as AnyErr, Result};
-use async_std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use futures::future::*;
 use futures::*;
 use quinn::{
   Certificate, CertificateChain, ClientConfig, ClientConfigBuilder, Endpoint, Incoming, PrivateKey,
   ServerConfig, ServerConfigBuilder, TransportConfig,
 };
-use snocat::util::framed::read_framed_json;
 use snocat::{
-  common::{
-    authentication::{AuthenticationClient, SimpleAckAuthenticationHandler},
-    MetaStreamHeader,
-  },
+  common::protocol::tunnel::{from_quinn_endpoint, Tunnel, TunnelIncomingType, TunnelSide},
+  util::{framed::read_framed_json, tunnel_stream::WrappedStream},
+};
+use snocat::{
+  common::{authentication::SimpleAckAuthenticationHandler, MetaStreamHeader},
   util::{self, validators::parse_socketaddr},
 };
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
@@ -24,6 +23,7 @@ use std::{
   sync::Arc,
   task::{Context, Poll},
 };
+use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub struct ClientArgs {
@@ -58,29 +58,27 @@ pub async fn client_main(config: ClientArgs) -> Result<()> {
     response_endpoint.bind(&"[::]:0".parse()?)? // Should this be IPv4 if the server is?
   };
 
-  let quinn::NewConnection {
-    connection,
-    bi_streams,
-    ..
-  } = {
+  let (tunnel, incoming) = {
     let connecting: Result<_, _> = endpoint
       .connect(&config.driver_host, &config.driver_san)
       .context("Connecting to server")?
       .await;
-    let mut connection = connecting.context("Finalizing connection to server...")?;
+    let connection = connecting.context("Finalizing connection to server...")?;
+    let (mut tunnel, mut incoming) = from_quinn_endpoint(connection, TunnelSide::Connect);
     // Authenticate tunnel
     let authenticator = SimpleAckAuthenticationHandler::new();
     let fake_shutdown_trigger = triggered::trigger();
-    ::snocat::common::authentication::perform_client_authentication(
+    ::snocat::common::authentication::perform_authentication(
       &authenticator,
-      &mut connection,
+      &mut tunnel,
+      &mut incoming,
       &fake_shutdown_trigger.1,
     )
-    .await?;
+    .await??;
     // Return successfully authenticated tunnel connection
-    connection
+    (tunnel, incoming)
   };
-  tracing::info!(remote = ?connection.remote_address(), "connected");
+  tracing::info!(remote = ?tunnel.addr(), "connected");
 
   tracing::debug!("Setting up stream handling...");
   {
@@ -101,9 +99,13 @@ pub async fn client_main(config: ClientArgs) -> Result<()> {
 
     tracing::info!("Stream listener installed; waiting for bistreams...");
     let mut connection_id: ConnectionId = 0;
-    bi_streams
+    incoming
+      .streams()
       .map_err(|e| e.into())
-      .try_for_each_concurrent(None, |(send, recv)| {
+      .try_for_each_concurrent(None, |stream: TunnelIncomingType| {
+        let stream = match stream {
+          TunnelIncomingType::BiStream(stream) => stream,
+        };
         tracing::info!("New bidirectional stream received, allocating handler coroutine...");
         handle_connection(
           {
@@ -111,7 +113,7 @@ pub async fn client_main(config: ClientArgs) -> Result<()> {
             connection_id += 1;
             tmp
           },
-          (send, recv),
+          stream,
           build_connection_handler,
         )
       })
@@ -135,12 +137,13 @@ type ProxyConnectionProvider<'a, 'b> = dyn Fn(
   ),
 >;
 
-#[tracing::instrument(skip(send, recv, build_proxy_connection), err)]
+#[tracing::instrument(skip(stream, build_proxy_connection), err)]
 async fn handle_connection(
   id: ConnectionId,
-  (mut send, mut recv): (quinn::SendStream, quinn::RecvStream),
+  stream: WrappedStream,
   build_proxy_connection: &ProxyConnectionProvider<'_, '_>,
 ) -> Result<()> {
+  let (mut recv, mut send) = tokio::io::split(stream);
   tracing::info!(
     target: "new connection",
     id = ?id
