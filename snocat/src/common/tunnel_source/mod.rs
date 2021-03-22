c8d0835e2772f876919a1485089aca0c1fee5fae
Zoey <Dessix@Dessix.net>
2021-03-22 15:02:04 -0700

    Added DynamicStreamSet for managing multiple endpoints or connections

--- snocat/src/common/tunnel_source/mod.rs
+++ snocat/src/common/tunnel_source/mod.rs
@@ -2,10 +2,15 @@
 // Licensed under the MIT license OR Apache 2.0
 //! Sources both listen- and connection-based tunnels
 
-use futures::stream::{BoxStream, Stream, StreamExt, TryStreamExt};
+use futures::stream::{BoxStream, Stream, StreamExt};
 use std::{net::SocketAddr, pin::Pin, task::Poll};
 
 use super::protocol::tunnel::{from_quinn_endpoint, BoxedTunnelPair, TunnelSide};
+use std::fmt::Debug;
+use std::hash::Hash;
+use std::sync::TryLockError;
+use std::task::Context;
+use tokio_stream::StreamMap;
 
 pub struct QuinnListenEndpoint<Session: quinn::crypto::Session> {
   bind_addr: SocketAddr,
@@ -56,5 +61,203 @@ where
   }
 }
 
-// /// Merges outputs from a runtime-editable set of endpoint ports
-// pub struct QuinnDynamicEndpointSet { }
+/// Structure used to hold boxed streams which have an ID associated with them
+///
+/// Primarily for use alongside StreamMap or DynamicStreamSet.
+pub struct NamedBoxedStream<Id, StreamItem> {
+  id: Id,
+  stream: BoxStream<'static, StreamItem>,
+}
+
+impl<Id, StreamItem> NamedBoxedStream<Id, StreamItem> {
+  pub fn new<TStream>(id: Id, stream: TStream) -> Self
+  where
+    TStream: Stream<Item = StreamItem> + Send + Sync + 'static,
+  {
+    Self::new_pre_boxed(id, stream.boxed())
+  }
+
+  pub fn new_pre_boxed(id: Id, stream: BoxStream<'static, StreamItem>) -> Self {
+    Self { id, stream }
+  }
+}
+
+impl<Id, StreamItem> Stream for NamedBoxedStream<Id, StreamItem>
+where
+  Id: Unpin,
+{
+  type Item = StreamItem;
+
+  fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    Stream::poll_next(Pin::new(&mut self.stream), cx)
+  }
+
+  fn size_hint(&self) -> (usize, Option<usize>) {
+    self.stream.size_hint()
+  }
+}
+
+impl<Id, StreamItem> std::fmt::Debug for NamedBoxedStream<Id, StreamItem>
+where
+  Id: Debug,
+{
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_struct(stringify!(DynamicConnection))
+      .field("id", &self.id)
+      .finish_non_exhaustive()
+  }
+}
+
+/// A set of connections / endpoints that can be updated dynamically, to allow runtime addition and
+/// removal of connections / "Tunnel sources" to those being handled by a tunnel server.
+pub type DynamicConnectionSet<Id> = DynamicStreamSet<Id, BoxedTunnelPair<'static>>;
+
+/// A strict wrapper for StreamMap that requires boxing of the items and handles locking for updates
+/// Can be used to merges outputs from a runtime-editable set of endpoint ports
+pub struct DynamicStreamSet<Id, TStream> {
+  // RwLock is semantically better here but poll_next is a mutation, so we'd have to
+  // trick it by using something like a refcell internally, losing most of the benefits.
+  //
+  // As this is to facilitate async, this is likely to be a near-uncontested mutex, but
+  // we use a std::sync::Mutex instead of an async one as we only expect to lock briefly.
+  streams: std::sync::Mutex<StreamMap<Id, NamedBoxedStream<Id, TStream>>>,
+}
+
+impl<Id, StreamItem> DynamicStreamSet<Id, StreamItem> {
+  pub fn new() -> Self {
+    Self {
+      streams: std::sync::Mutex::new(StreamMap::new()),
+    }
+  }
+
+  pub fn attach(
+    &self,
+    source: NamedBoxedStream<Id, StreamItem>,
+  ) -> Option<NamedBoxedStream<Id, StreamItem>>
+  where
+    Id: Clone + Hash + Eq,
+  {
+    let mut streams = self.streams.lock().expect("Mutex poisoned");
+    streams.insert(source.id.clone(), source)
+  }
+
+  pub fn attach_stream(
+    &self,
+    id: Id,
+    source: BoxStream<'static, StreamItem>,
+  ) -> Option<NamedBoxedStream<Id, StreamItem>>
+  where
+    Id: Clone + Hash + Eq,
+  {
+    let endpoint = NamedBoxedStream::new_pre_boxed(id.clone(), source);
+    self.attach(endpoint)
+  }
+
+  pub fn detach(&self, id: &Id) -> Option<NamedBoxedStream<Id, StreamItem>>
+  where
+    Id: Hash + Eq,
+  {
+    let mut streams = self.streams.lock().expect("Mutex poisoned");
+    streams.remove(id)
+  }
+}
+
+impl<Id, StreamItem> Stream for DynamicStreamSet<Id, StreamItem>
+where
+  Id: Clone + Unpin,
+{
+  type Item = (Id, StreamItem);
+
+  fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    // Use try_lock to ensure that we don't deadlock in a single-threaded async scenario
+    let mut streams = match self.streams.try_lock() {
+      Ok(s) => s,
+      Err(TryLockError::WouldBlock) => {
+        // Queue for another wake, to retry the mutex; essentially, yield for other async
+        // Note that this effectively becomes a spin-lock if the mutex is held while the
+        // async runtime has nothing else to work on.
+        cx.waker().wake_by_ref();
+        return Poll::Pending;
+      }
+      Err(TryLockError::Poisoned(poison)) => Err(poison).expect("Lock poisoned"),
+    };
+    Stream::poll_next(Pin::new(&mut *streams), cx)
+  }
+
+  // Size is hintable but slow to calculate and only useful if all sub-stream hints are precise
+  // Implement this only if the maintainability cost of a membership-update driven design is lower
+  // than that of the performance cost of doing so. Also consider the cost of mutex locking.
+  // fn size_hint(&self) -> (usize, Option<usize>) { (0, None) }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{DynamicStreamSet, NamedBoxedStream};
+  use crate::common::protocol::tunnel::BoxedTunnelPair;
+  use futures::task::Context;
+  use futures::{future, stream, FutureExt, Stream, StreamExt};
+  use std::collections::HashSet;
+  use std::iter::FromIterator;
+  use std::pin::Pin;
+
+  #[tokio::test]
+  async fn add_and_remove() {
+    let set = DynamicStreamSet::<u32, char>::new();
+    let a = stream::iter(vec!['a']).boxed();
+    let b = stream::iter(vec!['b']).boxed();
+    let c = stream::iter(vec!['c']).boxed();
+    set
+      .attach_stream(1u32, a)
+      .expect_none("Must attach to blank");
+    set
+      .attach_stream(2u32, b)
+      .expect_none("Must attach to non-blank with new key");
+    let mut replaced_b = set
+      .attach_stream(2u32, c)
+      .expect("Must overwrite keys and return an old one");
+    let mut detached_a = set.detach(&1u32).expect("Must detach fresh keys by ID");
+    let mut detached_c = set.detach(&2u32).expect("Must detach replaced keys by ID");
+    assert_eq!(detached_a.id, 1u32);
+    assert_eq!(
+      detached_a.stream.next().await.expect("Must have item"),
+      'a',
+      "Fresh-key stream identity mismatch"
+    );
+    assert_eq!(replaced_b.id, 2u32);
+    assert_eq!(
+      replaced_b.stream.next().await.expect("Must have item"),
+      'b',
+      "Replaced stream identity mismatch"
+    );
+    assert_eq!(detached_c.id, 2u32);
+    assert_eq!(
+      detached_c.stream.next().await.expect("Must have item"),
+      'c',
+      "Replacement stream identity mismatch"
+    );
+  }
+
+  #[tokio::test]
+  async fn poll_contents() {
+    let set = DynamicStreamSet::<u32, char>::new();
+    let a = stream::iter(vec!['a']).boxed();
+    let b = stream::iter(vec!['b']).boxed();
+    let c = stream::iter(vec!['c']).boxed();
+    set
+      .attach_stream(1u32, a)
+      .expect_none("Must attach to blank");
+    set
+      .attach_stream(2u32, b)
+      .expect_none("Must attach to non-blank with new key");
+    set
+      .attach_stream(2u32, c)
+      .expect("Must replace existing keys");
+    // We use a hashset because we don't specify a strict ordering, that's internal to StreamMap
+    let results = set.collect::<HashSet<_>>().await;
+    // Note that 'b' must not occur here because we've detached it
+    assert_eq!(
+      results,
+      HashSet::from_iter(vec![(1, 'a'), (2, 'c')].into_iter())
+    );
+  }
+}
