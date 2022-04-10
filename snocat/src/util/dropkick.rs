7d931947da8d602e9ec07f634976be0b8c89bad2
Zoey <Dessix@Dessix.net>
2022-04-10 14:46:36 -0700

    Added deadlock prevention and tests to Arc-Mutex-Option Dropkicks

--- snocat/src/util/dropkick.rs
+++ snocat/src/util/dropkick.rs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license OR Apache 2.0
 #![deny(dead_code, unused_imports)]
 
-use std::sync::{Arc, Mutex};
+use std::sync::{atomic::AtomicUsize, Arc, Mutex};
 
 /// A trait describing the concept of "dropkicking", in an allusion to percusive maintenance.
 ///
@@ -63,14 +63,23 @@ where
 impl<T> Dropkick<Arc<Mutex<Option<T>>>>
 where
   T: DropkickSync,
+  Arc<Mutex<Option<T>>>: DropkickSync,
 {
   /// Takes the content out of the mutex, preventing [DropkickSync::drop_kick] from being called on its content within an Arc
+  ///
+  /// Blocking; Locks the [std::sync::Mutex] synchronously, and will deadlock if the lock is held elsewhere by the same thread.
   pub fn counter_take_mutex(&self) -> Option<T> {
     self
       .inner
       .as_ref()
-      .and_then(|inner| inner.lock().ok())
-      .and_then(|mut inner| inner.take())
+      .and_then(|inner| match inner.lock().ok() {
+        Some(mut lock) => {
+          let value = lock.take();
+          drop(lock);
+          value
+        }
+        None => None,
+      })
   }
 }
 
@@ -212,7 +221,7 @@ where
   }
 }
 
-/// Mutexes drop their contents by locking
+/// Mutexes drop their contents by unwrapping and clearing
 impl<T> DropkickSync for std::sync::Mutex<T>
 where
   T: DropkickSync,
@@ -224,14 +233,72 @@ where
   }
 }
 
-/// Arc-Mut-Opts drop their contents by locking, emptying the Option, and dropping it
-impl<T> DropkickSync for std::sync::Arc<std::sync::Mutex<Option<T>>>
+/// Records the number of active mutex background tasks/threads
+static DROPKICK_MUTEX_OUTSTANDING_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+fn dropkick_mutex_background_task<T>(counter: &'static AtomicUsize, mutarc: Arc<Mutex<Option<T>>>)
 where
   T: DropkickSync,
 {
+  tracing::trace_span!("Dropkick Mutex background cleanup").in_scope(|| {
+    let active_drop_count = counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed) + 1;
+    tracing::trace!(
+      "Dropkick Mutex background cleanup thread started due to contention, active count: {}",
+      active_drop_count,
+    );
+    let res = mutarc.lock().expect("Dropkick Mutex poisoned").take();
+    <Option<T> as DropkickSync>::dropkick(res);
+    let active_drop_count = counter.fetch_sub(1, std::sync::atomic::Ordering::Relaxed) - 1;
+    tracing::trace!(
+      "Dropkick Mutex background cleanup thread completed, remaining active count: {}",
+      active_drop_count,
+    );
+  });
+}
+
+/// Arc-Mut-Opts drop their contents by locking, emptying the Option, and executing the drop
+///
+/// There are three paths involved- each varying in optimism:
+///
+/// - The Arc is the only reference, so we can [function@Arc::try_unwrap] it,
+///   then dispatch to the "owned mutex" implementation of [DropkickSync].
+///
+/// - The Mutex successfully locks immediately during the synchronous call, so we can use
+///   [Option::take], then dispatch to the Option implementation of [DropkickSync].
+///
+/// - A blocking, background task is instantiated to lock the mutex asynchronously, eventually.
+///   This path may deadlock if the mutex is held by another source indefinitely, but -
+///   as it is on another thread - will not hang the dropping thread directly.
+impl<T> DropkickSync for std::sync::Arc<std::sync::Mutex<Option<T>>>
+where
+  T: Send + DropkickSync + 'static,
+{
   fn dropkick(self) {
-    if let Some(inner) = self.lock().ok().map(|mut x| x.take()) {
-      DropkickSync::dropkick(inner);
+    let this = match Arc::try_unwrap(self) {
+      // Optimistic path- if we're the only reference, we can avoid creating locking overhead
+      Ok(inner) => return <Mutex<Option<T>> as DropkickSync>::dropkick(inner),
+      Err(this) => this,
+    };
+    // Attempt try_lock, then take the Option contents if immediately successful
+    let taken = match this.try_lock() {
+      Ok(mut lock) => {
+        let taken: Option<T> = lock.take();
+        // By exiting scope, we exit the lock context before executing the dropper,
+        // as we want to minimize lock overlap time and have already taken the contents
+        drop(lock);
+        // Delegate using a new Dropkick to ensure all paths must drop it or opt-out
+        Some(Dropkick::new(taken))
+      }
+      Err(std::sync::TryLockError::Poisoned(poisoned)) => {
+        panic!("Dropkick Lock was poisoned: {:?}", poisoned)
+      }
+      Err(std::sync::TryLockError::WouldBlock) => None,
+    };
+    // By only operating on `None`, we allow our subsequent drop call to run on the content after the scope ends
+    if taken.is_none() {
+      tokio::task::spawn_blocking(move || {
+        dropkick_mutex_background_task(&DROPKICK_MUTEX_OUTSTANDING_COUNT, this)
+      });
     }
   }
 }
@@ -247,6 +314,8 @@ where
 
 #[cfg(test)]
 mod tests {
+  use std::sync::{atomic::AtomicBool, Arc, Mutex};
+
   use super::{Dropkick, DropkickSync};
 
   #[repr(transparent)]
@@ -292,6 +361,110 @@ mod tests {
     assert!(
       !m,
       "Callback Dropkick must not call drop_kick when consumed"
+    );
+  }
+
+  /// Verifies that an exclusively-held Arc-Mut-Opt calls no async-runtime functionality in Dropkick
+  ///
+  /// This ensures we take a fast-path in dropping, but cannot verify whether
+  /// it is [Arc::try_unwrap] or [Mutex::try_lock] that is selected for use.
+  #[test]
+  fn dropkick_exclusive_arc_try_unwrap_optimization() {
+    let m = Arc::new(AtomicBool::new(false));
+    let target_arcmutopt: Arc<Mutex<_>> = Arc::new(Mutex::new(Some({
+      let m = m.clone();
+      move || m.store(true, std::sync::atomic::Ordering::Relaxed)
+    })));
+    let dropkick = Dropkick::new(target_arcmutopt);
+
+    assert!(
+      !m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must not execute until dropped"
+    );
+    drop(dropkick);
+    assert!(
+      m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must call drop_kick"
+    );
+  }
+
+  /// Verifies that an externally-held but unlocked Arc-Mut-Opt calls no async-runtime functionality in Dropkick
+  ///
+  /// This ensures we take the try_lock fast-path in dropping.
+  #[test]
+  fn dropkick_exclusive_mutex_try_lock_optimization() {
+    let m = Arc::new(AtomicBool::new(false));
+    let target_arcmutopt: Arc<Mutex<_>> = Arc::new(Mutex::new(Some({
+      let m = m.clone();
+      move || m.store(true, std::sync::atomic::Ordering::Relaxed)
+    })));
+    let secondary_hold: Arc<Mutex<_>> = Arc::clone(&target_arcmutopt);
+    let dropkick = Dropkick::new(target_arcmutopt);
+
+    assert!(
+      !m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must not execute until dropped"
+    );
+    drop(dropkick);
+    assert!(
+      m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must call drop_kick"
+    );
+    drop(secondary_hold);
+  }
+
+  #[tokio::test]
+  async fn dropkick_mutex_background_task() {
+    let m = Arc::new(AtomicBool::new(false));
+    let target_arcmutopt = Arc::new(Mutex::new(Some({
+      let m = m.clone();
+      move || m.store(true, std::sync::atomic::Ordering::Relaxed)
+    })));
+    let secondary_hold: Arc<Mutex<_>> = Arc::clone(&target_arcmutopt);
+    let dropkick = Dropkick::new(target_arcmutopt);
+    let held_lock = secondary_hold.lock().unwrap();
+
+    assert!(
+      !m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must not execute until dropped"
+    );
+    drop(dropkick);
+    assert!(
+      !m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick cannot have dropped while still locked"
+    );
+    drop(held_lock);
+    for _ in 0..100 {
+      if m.load(std::sync::atomic::Ordering::Relaxed) {
+        break;
+      }
+      tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    }
+    assert!(
+      m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must call drop_kick on background thread after it is unlocked"
+    );
+  }
+
+  /// Verifies that an Arc-Mut-Opt [Dropkick] can have its contents removed cleanly to conditionally prevent drop callbacks
+  #[test]
+  fn dropkick_counter_take_mutex() {
+    let m = Arc::new(AtomicBool::new(false));
+    let target_arcmutopt: Arc<Mutex<_>> = Arc::new(Mutex::new(Some({
+      let m = m.clone();
+      move || m.store(true, std::sync::atomic::Ordering::Relaxed)
+    })));
+    let dropkick = Dropkick::new(target_arcmutopt);
+
+    drop(dropkick.counter_take_mutex());
+    assert!(
+      !m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick::counter_take_mutex must not invoke dropkick event"
+    );
+    drop(dropkick);
+    assert!(
+      !m.load(std::sync::atomic::Ordering::Relaxed),
+      "Dropkick must not invoke counter_take_mutex-removed target"
     );
   }
 }
