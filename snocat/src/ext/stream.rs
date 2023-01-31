c7496eaa8f813489d0a0714d43099bda768ded8b
Zoey <Dessix@Dessix.net>
2023-01-30 20:11:35 -0800

    Implemented a TaskTracker wrapper for BoundCounter

--- snocat/src/ext/stream.rs
+++ snocat/src/ext/stream.rs
@@ -2,7 +2,10 @@
 // Licensed under the MIT license OR Apache 2.0
 
 mod bound_counter {
-  use crate::util::dropkick::{Dropkick, DropkickSync};
+  use crate::{
+    ext::future::TaskTracker,
+    util::dropkick::{Dropkick, DropkickSync},
+  };
   use std::sync::Arc;
 
   const BASELINE_COUNT: usize = 1; // number of counters present when true count is zero
@@ -55,6 +58,296 @@ mod bound_counter {
       Self::update_current_count(&self.notifier, &self.counter_holder);
     }
   }
+
+  enum BoundCounterTrackerState {
+    /// Tracked task has yet to be polled, so tracking has not yet been established
+    Unregistered {
+      notifier: Arc<tokio::sync::watch::Sender<usize>>,
+      counter_holder: Arc<Arc<()>>,
+    },
+    /// Tracking is registered; dropping will destroy this registration.
+    /// Further calls to "TaskTracker::register" will yield the same registration,
+    /// which allows us to safely double-register, which in turn gives us the
+    /// option to "pre-register" for when unpolled tasks need tracked as well.
+    Registered(BoundCounter),
+  }
+
+  impl BoundCounterTrackerState {
+    pub fn new(
+      notifier: Arc<tokio::sync::watch::Sender<usize>>,
+      counter_holder: Arc<Arc<()>>,
+    ) -> Self {
+      Self::Unregistered {
+        notifier,
+        counter_holder,
+      }
+    }
+  }
+
+  // Wrap the inner type to keep it private to the module,
+  // and to isolate the Drop behaviour constraint surface.
+  /// Tracks the count of a task by pushing notifications of the latest outstanding count to a [tokio::sync::watch] channel.
+  #[repr(transparent)]
+  pub struct BoundCounterTracker(BoundCounterTrackerState);
+
+  impl BoundCounterTracker {
+    pub fn new(
+      notifier: Arc<tokio::sync::watch::Sender<usize>>,
+      counter_holder: Arc<Arc<()>>,
+    ) -> Self {
+      Self(BoundCounterTrackerState::new(notifier, counter_holder))
+    }
+
+    /// Tracks immediately rather than waiting for an initial poll
+    pub fn new_preregistered(
+      notifier: Arc<tokio::sync::watch::Sender<usize>>,
+      counter_holder: Arc<Arc<()>>,
+    ) -> Self {
+      Self::new(notifier, counter_holder).register_sync()
+    }
+
+    fn register_sync(mut self) -> Self {
+      match &mut self.0 {
+        BoundCounterTrackerState::Registered(_) => self,
+        BoundCounterTrackerState::Unregistered {
+          notifier,
+          counter_holder,
+        } => Self(BoundCounterTrackerState::Registered(BoundCounter::new(
+          &notifier,
+          &counter_holder,
+        ))),
+      }
+    }
+  }
+
+  impl TaskTracker for BoundCounterTracker {
+    type RegistrationFuture = futures::future::Ready<Self>;
+
+    fn register(self) -> Self::RegistrationFuture {
+      futures::future::ready(self.register_sync())
+    }
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use std::{sync::Arc, task::Poll, time::Duration};
+
+    use futures::FutureExt;
+
+    use crate::ext::{future::FutureExtExt, stream::bound_counter::BoundCounterTracker};
+
+    #[tokio::test]
+    async fn bound_counter_tracking_preregistered() {
+      use tokio::sync::{mpsc::unbounded_channel, watch};
+      let (watch_tx, mut watch_rx) = watch::channel(Default::default());
+      let (updater, counter_holder) = (Arc::new(watch_tx), Arc::new(Arc::new(())));
+      let (queue_tx, mut queue_rx) = unbounded_channel();
+      const COUNT: usize = 100usize;
+      for i in 0..COUNT {
+        queue_tx
+          .send(BoundCounterTracker::new_preregistered(
+            updater.clone(),
+            counter_holder.clone(),
+          ))
+          .ok()
+          .expect("tracker send failed");
+        assert_eq!(
+          *watch_rx.borrow(),
+          i + 1,
+          "Tracked value was out of sync with allocated trackers in countup"
+        );
+      }
+      drop(queue_tx);
+      println!("Count-up complete");
+      let mut outstanding = COUNT;
+      while let Some(item) = queue_rx.recv().await {
+        assert!(outstanding > 0, "Received more than expected?");
+        outstanding -= 1;
+        let expectation = watch_rx.changed();
+        drop(item);
+        print!("Waiting for a notification, or 5 seconds... ");
+        let reality = expectation
+          .map(|_| ())
+          .poll_until(tokio::time::sleep(Duration::from_secs(5)).boxed())
+          .await;
+        reality.expect("No change notification received after expectation in notifier");
+        println!("Notified!");
+      }
+      assert_eq!(outstanding, 0, "Received a different amount than expected?");
+      println!("Countdown complete");
+      assert_eq!(
+        &*watch_rx.borrow(),
+        &0,
+        "Tracked value did not return to 0 after countdown"
+      );
+    }
+
+    /// Verifies that tracked items clean up their tracking registration if dropped mid-execution
+    #[tokio::test]
+    async fn bound_counter_tracking_lazy_in_progress_drop() {
+      #[derive(Debug, PartialEq, Eq)]
+      struct TestResultValue;
+
+      use tokio::sync::watch;
+      let (watch_tx, mut watch_rx) = watch::channel(Default::default());
+      let (updater, counter_holder) = (Arc::new(watch_tx), Arc::new(Arc::new(())));
+
+      const COUNT: usize = 100usize;
+
+      assert_eq!(
+        *watch_rx.borrow(),
+        0,
+        "Tracked value should be zero before count-up"
+      );
+      let barrier = Arc::new(tokio::sync::Notify::new());
+      let mut pool = Vec::new();
+      for _ in 0..COUNT {
+        let tracker = BoundCounterTracker::new(updater.clone(), counter_holder.clone());
+        let barrier = barrier.clone();
+        pool.push(Box::pin(
+          async move {
+            barrier.notified().await;
+            TestResultValue
+          }
+          .track(tracker),
+        ));
+        assert_eq!(
+          *watch_rx.borrow(),
+          0,
+          "Tracked value should not go up in lazy-registered count-up"
+        );
+      }
+      println!("Lazy-registered count-up construction complete");
+
+      let count_up_notification = watch_rx.changed();
+      // Poll all tasks once
+      for item in pool.iter_mut() {
+        let res = futures::poll!(item);
+        assert_eq!(
+          res,
+          Poll::Pending,
+          "Test futures must not complete until the notification is prepared"
+        );
+      }
+      println!("Awaiting lazy-registered count-up side-effects...");
+      let reality = count_up_notification
+        .map(|_| ())
+        .poll_until(tokio::time::sleep(Duration::from_secs(5)).boxed())
+        .await;
+      reality.expect("No change notification received after expectation in count-up notifier");
+      println!("Lazy-registered count-up side-effects noted!");
+
+      // None of the futures in the pool have actually returned their values;
+      // Delete them one by one and verify that a notification occurs for each.
+      let mut outstanding = COUNT;
+      for fut in pool.into_iter() {
+        assert!(outstanding > 0, "Received more than expected?");
+        outstanding -= 1;
+        let expectation = watch_rx.changed();
+        drop(fut);
+        print!("Waiting for a notification, or 5 seconds... ");
+        let reality = expectation
+          .map(|_| ())
+          .poll_until(tokio::time::sleep(Duration::from_secs(5)).boxed())
+          .await;
+        reality.expect("No change notification received after expectation in notifier");
+        println!("Notified!");
+      }
+      assert_eq!(outstanding, 0, "Received a different amount than expected?");
+      println!("Countdown complete");
+
+      assert_eq!(
+        &*watch_rx.borrow(),
+        &0,
+        "Tracked value did not return to 0 after countdown"
+      );
+    }
+
+    #[tokio::test]
+    async fn bound_counter_tracking_lazy() {
+      #[derive(Debug, PartialEq, Eq)]
+      struct TestResultValue;
+
+      use tokio::sync::watch;
+      let (watch_tx, mut watch_rx) = watch::channel(Default::default());
+      let (updater, counter_holder) = (Arc::new(watch_tx), Arc::new(Arc::new(())));
+
+      const COUNT: usize = 100usize;
+
+      assert_eq!(
+        *watch_rx.borrow(),
+        0,
+        "Tracked value should be zero before count-up"
+      );
+      let barrier = Arc::new(tokio::sync::Notify::new());
+      let mut pool = Vec::new();
+      for _ in 0..COUNT {
+        let tracker = BoundCounterTracker::new(updater.clone(), counter_holder.clone());
+        let barrier = barrier.clone();
+        pool.push(Box::pin(
+          async move {
+            barrier.notified().await;
+            TestResultValue
+          }
+          .track(tracker),
+        ));
+        assert_eq!(
+          *watch_rx.borrow(),
+          0,
+          "Tracked value should not go up in lazy-registered count-up"
+        );
+      }
+      println!("Lazy-registered count-up construction complete");
+
+      let count_up_notification = watch_rx.changed();
+      // Poll all tasks once
+      for item in pool.iter_mut() {
+        let res = futures::poll!(item);
+        assert_eq!(
+          res,
+          Poll::Pending,
+          "Test futures must not complete until the notification is prepared"
+        );
+      }
+      println!("Awaiting lazy-registered count-up side-effects...");
+      let reality = count_up_notification
+        .map(|_| ())
+        .poll_until(tokio::time::sleep(Duration::from_secs(5)).boxed())
+        .await;
+      reality.expect("No change notification received after expectation in count-up notifier");
+      println!("Lazy-registered count-up side-effects noted!");
+
+      // Tell queued futures they can now complete
+
+      barrier.notify_waiters();
+      let mut outstanding = COUNT;
+      for fut in pool.into_iter() {
+        assert!(outstanding > 0, "Received more than expected?");
+        outstanding -= 1;
+        let expectation = watch_rx.changed();
+        let res = fut
+          .poll_until(tokio::time::sleep(Duration::from_secs(5)).boxed())
+          .await
+          .expect("Timed out waiting for future completion");
+        assert_eq!(res, TestResultValue, "Future must produce the test result");
+        print!("Waiting for a notification, or 5 seconds... ");
+        let reality = expectation
+          .map(|_| ())
+          .poll_until(tokio::time::sleep(Duration::from_secs(5)).boxed())
+          .await;
+        reality.expect("No change notification received after expectation in notifier");
+        println!("Notified!");
+      }
+      assert_eq!(outstanding, 0, "Received a different amount than expected?");
+      println!("Countdown complete");
+
+      assert_eq!(
+        &*watch_rx.borrow(),
+        &0,
+        "Tracked value did not return to 0 after countdown"
+      );
+    }
+  }
 }
 
 mod stream_ext_ext {
@@ -64,6 +357,10 @@ mod stream_ext_ext {
     future::{BoxFuture, Future, FutureExt},
     stream::{StreamExt, TryForEachConcurrent, TryStream, TryStreamExt},
   };
+
+  use crate::ext::future::FutureExtExt;
+
+  use super::bound_counter::BoundCounterTracker;
   pub trait StreamExtExt: StreamExt + private::Sealed {
     // TODO: Replace with https://docs.rs/futures/latest/futures/stream/struct.FuturesUnordered.html#method.len
     // TODO: Notify subscribers of changes when `push` or `next` return; "pull" by polling on Next or the upstream future.
@@ -92,15 +389,9 @@ mod stream_ext_ext {
       self.try_for_each_concurrent(
         limit,
         Box::new(move |ok| {
-          let bound_counter = super::bound_counter::BoundCounter::new(&updater, &counter_holder);
-          let fut = f(ok);
-          async move {
-            let bound_counter = bound_counter;
-            let res = fut.await;
-            drop(bound_counter);
-            res
-          }
-          .boxed()
+          let tracker =
+            BoundCounterTracker::new_preregistered(updater.clone(), counter_holder.clone());
+          f(ok).track(tracker).boxed()
         }),
       )
     }
